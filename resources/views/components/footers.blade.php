59a7b04175dbe3d15f875a2468d6d6eeb076a43b
njammy <njammypatchakwejovani@gmail.com>
2022-11-24 07:59:21 +0100

    footer ok

--- resources/views/components/footers.blade.php
+++ resources/views/components/footers.blade.php
@@ -1,3 +1,85 @@
-@section ('body') 
-Footers
-@endsection
+<footer>
+    <div class="foo-first">
+        <div class="foo-icon">
+            <a href="">
+                <img src="/images/share.png" alt="">
+            </a>
+        </div>
+        <h3>Partagez cette page</h3>
+        <div class="foo-icon">
+            <a href="">
+                <img src="/images/face.png" alt="">
+            </a>
+        </div>
+        <div class="foo-icon">
+            <a href="">
+                <img src="/images/twit.png" alt="">
+            </a>
+        </div>
+        <div class="foo-icon">
+            <a href="">
+                <img src="/images/insta.png" alt="">
+            </a>
+        </div>
+        <div class="foo-icon">
+            <a href="">
+                <img src="/images/link.png" alt="">
+            </a>
+        </div>
+    </div>
+    <div class="foo-second">
+        <div class="foo-second-bloc">
+            <h2 class="subtitle-primary">Connectez vous à nous</h2>
+            <div class="paragraph">Nous cherchons toujours à garder le contact avec les personnes pour qui un avenir durable est important.</div>
+            <div class="foo-second-social">
+                <div class="foo-icon">
+                    <a href="">
+                        <img src="/images/face-up.png" alt="">
+                    </a>
+                </div>
+                <div class="foo-icon">
+                    <a href="">
+                        <img src="/images/twit-up.png" alt="">
+                    </a>
+                </div>
+                <div class="foo-icon">
+                    <a href="">
+                        <img src="/images/insta-up.png" alt="">
+                    </a>
+                </div>
+                <div class="foo-icon">
+                    <a href="">
+                        <img src="/images/link-up.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="foo-second-bloc">
+            <h2 class="subtitle-primary">Nous joindre</h2>
+            <div class="paragraph">Contactez Unilever et ses équipes de spécialistes ou trouvez des contacts du monde entier.</div>
+            <div class="foo-join-master">
+                <div class="foo-join">Nous Joindre</div> <div class="foo-join foo-join-icon"> <img src="/images/play.png" alt=""> </div>
+            </div>
+        </div>
+    </div>
+    <div class="foo-third">
+        <div class="foo-sub-first">
+            <div>
+                <div class="foo-logo-label">Unilever</div>
+            </div>
+            <div>© Unilever 2022</div>
+            <div class="link-foo-group">
+                <a class="footer-third-link" href="">Contactez nous</a>
+                <a class="footer-third-link" href="">Qu'y a-t-il dans nos produits</a>
+                <a class="footer-third-link" href="">Mentions légales</a>
+                <a class="footer-third-link" href="">Accessibilité</a>
+                <a class="footer-third-link" href="">Avis relatifs aux Cookies</a>
+                <a class="footer-third-link" href="">Avis de Confidentialité</a>
+                <a class="footer-third-link" href="">SitesMaps</a>
+            </div>
+        </div>
+        <div class="foo-sub-second">
+            <img src="https://www.unilever.fr/assets/8ada3bf5.webp" />
+        </div>
+    </div>
+</footer>
